32347d5bb07ad2521dcd500e0bdd2fed54ffad1a
zhang1998 <2764207312@qq.com>
2019-12-27 15:35:31 +0800

    第一次成功运行UI

---
+++ mycode/frombook(复件)/main.h
@@ -0,0 +1,5 @@
+﻿#ifndef MAIN_H_
+#define MAIN_H_
+
+
+#endif /* MAIN_H_ */
---
+++ mycode/frombook/baby.h
@@ -0,0 +1,28 @@
+﻿#ifndef __BABY_H__
+#define __BABY_H__
+#include <glib-object.h>
+#define BABY_TYPE (baby_get_type())
+#define BABY(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), BABY_TYPE, baby))	// 定义对象转换宏
+typedef struct _Baby Baby;												// 定义Baby结构
+typedef struct _BabyClass BabyClass;										// 定义类结构
+struct _Baby {
+   GObject parent;														// 定义类成员，父类
+   gint age;
+   gchar *name;
+   void (*cry)(void);														// 类的行为
+};
+struct _BabyClass {														// 定义类结构
+   GObjectClass parent_class;
+   void (*baby_born)(void);												//类似于C++构造函数
+};
+GType baby_get_type(void);
+Baby* baby_new(void);
+int baby_get_age(Baby *baby);
+void baby_set_age(Baby *baby, int age);
+char* baby_get_name(Baby *baby);
+void baby_set_name(Baby *baby, char *name);
+Baby* baby_new_with_name(gchar *name);
+Baby* baby_new_with_age(gint age);
+Baby* baby_new_with_name_and_age(gchar *name, gint age);
+void baby_info(Baby *baby);
+#endif
